721973fe9593095eb5c540753db53007fff2ee87
holdn2 <yoocy01@gmail.com>
2026-07-06 11:21:23 +0900

    style: 주석 추가

--- src/app/admin/(with-sidebar)/worktime/page.tsx
+++ src/app/admin/(with-sidebar)/worktime/page.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 
+// 시간표에 대해서 일반 사용자에서는 schedule이고 관리자에서는 work time으로 네이밍함. 추후 하나로 통일 예정
+
 export default function WorktimePage() {
   return (
     <div className="flex-1 px-10 py-8">
